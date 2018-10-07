5329768c6feb99625c0af4aca9abbecfe8384028
Marta Niemiec <niemiec.marta@hotmail.com>
2018-10-08 00:24:00 +0200

    deleted useless files and created Card component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';  // "robots" is inside of {} because in robots.js "export" isn't "default" so it could have multiple exports
 
-ReactDOM.render(<Hello greeting={'Hello ' + 'React Ninja'}/>,   document.getElementById('root'));   // "greeting" - created props(property); can be used in the component
+ReactDOM.render(<div>
+                    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+                    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+                    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+                </div>, document.getElementById('root'));   //
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
