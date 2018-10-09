5de95567cc38e56920e549c27cbbedcb3fb8b2f4
Marta Niemiec <niemiec.marta@hotmail.com>
2018-10-09 11:04:59 +0200

    added componentDidMount() with API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';   // React.Component = {Component}
 import CardList from './CardList';  //parent of Card component
 import SearchBox from './SearchBox';
-import { robots } from './robots';  // "robots" is inside of {} because in robots.js "export" isn't "default" so it could have multiple exports
 import './App.css';
 
 class App extends Component {
@@ -12,12 +11,19 @@ class App extends Component {
     constructor() {
         super() // calls the constructor of Component
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '' 
         }
     }
 
     // With anything that comes from React, so constructor and render, are pre-built in React, any time we make our own methods on a component, we have to use arrow functions, and this makes sure that the "this" value is according to where it was created, which is the "App"
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')  //fetch is a part of the window object
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
     onSearchChange = (event) => {   
         this.setState({ searchfield: event.target.value })
     }
@@ -26,13 +32,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
     
 }
