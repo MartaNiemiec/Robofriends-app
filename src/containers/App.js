b7c581f0535151a5381003367bbde1bde1f59f28
Marta Niemiec <niemiec.marta@hotmail.com>
2018-10-09 23:14:48 +0200

    created ErrorBoundry  component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';   // React.Component = {Component}
 import CardList from '../components/CardList';  //parent of Card component
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'; 
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
 class App extends Component {
 
-// ====== STATE ======
-// state is something that can change and affect the app; 
-// state lives in the parent component
+    // ====== STATE ======
+    // state is something that can change and affect the app; 
+    // state lives in the parent component
     constructor() {
         super() // calls the constructor of Component
         this.state = {
@@ -16,8 +17,7 @@ class App extends Component {
             searchfield: '' 
         }
     }
-
-    // With anything that comes from React, so constructor and render, are pre-built in React, any time we make our own methods on a component, we have to use arrow functions, and this makes sure that the "this" value is according to where it was created, which is the "App"
+    
 
     componentDidMount() {
         fetch('http://jsonplaceholder.typicode.com/users')  //fetch is a part of the window object
@@ -25,9 +25,12 @@ class App extends Component {
         .then(users => this.setState({robots: users}));
     }
 
+    
+    // With anything that comes from React, so constructor and render, are pre-built in React, any time we make our own methods on a component, we have to use arrow functions, and this makes sure that the "this" value is according to where it was created, which is the "App"
     onSearchChange = (event) => {   
         this.setState({ searchfield: event.target.value })
     }
+
 
     render() {
         const {robots, searchfield} = this.state;
@@ -41,12 +44,14 @@ class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
     }
-    
 }
+
 
 export default App;
