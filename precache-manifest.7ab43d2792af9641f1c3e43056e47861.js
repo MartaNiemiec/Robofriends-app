f0f348e7db4318369a29cc6da56a375f601a17cd
Marta Niemiec <niemiec.marta@hotmail.com>
2018-11-17 14:04:36 +0100

    Updates

---
+++ precache-manifest.7ab43d2792af9641f1c3e43056e47861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d538c99c1ec95bef1223",
+    "url": "/Robofriends-app/static/js/runtime~main.d538c99c.js"
+  },
+  {
+    "revision": "8fc0ede7e8f9a4f83365",
+    "url": "/Robofriends-app/static/js/main.8fc0ede7.chunk.js"
+  },
+  {
+    "revision": "6e0f73fe7996a9f6a0f2",
+    "url": "/Robofriends-app/static/js/1.6e0f73fe.chunk.js"
+  },
+  {
+    "revision": "8fc0ede7e8f9a4f83365",
+    "url": "/Robofriends-app/static/css/main.e55c6a0c.chunk.css"
+  },
+  {
+    "revision": "6e0f73fe7996a9f6a0f2",
+    "url": "/Robofriends-app/static/css/1.55c5aa15.chunk.css"
+  },
+  {
+    "revision": "05f43a1ff8ad6b3b912abd162fe15c37",
+    "url": "/Robofriends-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bfdd11b1e3b304419f25be9861158a9.js"
+  "/Robofriends-app/precache-manifest.7ab43d2792af9641f1c3e43056e47861.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Robofriends-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
