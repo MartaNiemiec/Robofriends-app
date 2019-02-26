bbaa7ec1768276d0047f95657a597546bfd19413
Marta Niemiec <niemiec.marta@hotmail.com>
2019-02-26 11:43:22 +0100

    change API url from "http" to "https"

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     
 
     componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')  //fetch is a part of the window object
+        fetch('https://jsonplaceholder.typicode.com/users')  //fetch is a part of the window object
         .then(response => response.json())  //response return a promise
         .then(users => this.setState({robots: users}));
     }
