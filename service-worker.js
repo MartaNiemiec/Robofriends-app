c207d3863f5909af8e4ebc71bfb92af80d7514bb
Marta Niemiec <niemiec.marta@hotmail.com>
2019-02-26 11:43:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Robofriends-app/precache-manifest.7ab43d2792af9641f1c3e43056e47861.js"
+  "/Robofriends-app/precache-manifest.34a926c31d054818e80fbffe56a6bec5.js"
 );
 
 workbox.clientsClaim();
